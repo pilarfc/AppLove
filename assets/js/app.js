c4500877c9ab1ff882da51265bd29b00c9cfa5d3
Pilar Figueroa <pilar.fc@outlook.com>
2017-04-17 12:23:31 -0500

    Ya realiza todos los puntos requeridos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,30 @@
+var modal = document.getElementById("myModal");
 
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('imagen1');
 var modalImg = document.getElementById("modalImagen1");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
+var captionText = document.getElementById("caption");
+var imagenes = [];
+var span = [];
+
+
+for (var i = 0; i < 9; i++){
+ imagenes[i]=document.getElementsByClassName("mostrar")[i];
+ imagenes[i].addEventListener("click", desplegar);
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+function desplegar(){
+ modal.style.display = "block";
+ modalImg.src = this.src;
+ captionText.innerHTML = this.alt;
+}
+
+for (var i = 0; i < 9; i++){
+ span[i]=document.getElementsByClassName("close")[0];
+ span[i].addEventListener("click",cerrar);
+}
+
+
+function cerrar() {
+ modal.style.display = "none";
 }
